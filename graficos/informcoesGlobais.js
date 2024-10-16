feaa69614d293f8cb07e192e9b3b591a682e48c5
tiagotuponi <128715693+tiagotuponi@users.noreply.github.com>
2024-10-16 10:11:05 -0300

    Update informcoesGlobais.js

--- graficos/informcoesGlobais.js
+++ graficos/informcoesGlobais.js
@@ -1,1 +1,9 @@
+   const url = ´"https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
 
+   async function visualizarinformcoesGlobais() {
+        const res = await fetch(url)
+        const dados = await res.json()
+        const .log(dados);
+   }
+
+   visualizarInformacoesGlobais()
